17d85626d005899d728404bd9c12b26b4aa5e9cb
Felix Lange <langefx@gmail.com>
2015-08-23 16:29:06 +0200

    fix #9 datGui example for firefox

--- example_datGui/bin/data/web/js/main.js
+++ example_datGui/bin/data/web/js/main.js
@@ -22,12 +22,12 @@ function setupSocket(){
 	// it decides what the websocket url is based on the broswer url
 	// e.g. https://mygreathost:9099 = wss://mygreathost:9099
 
-	if (BrowserDetect.browser == "Firefox") {
-		socket = new MozWebSocket(get_appropriate_ws_url());
-	} else {
+	// if (BrowserDetect.browser == "Firefox") {
+	// 	socket = new MozWebSocket(get_appropriate_ws_url());
+	// } else {
 		socket = new WebSocket(get_appropriate_ws_url());
 		//socket = new WebSocket("ws://localhost:9092");
-	}
+	// }
 	
 	// open
 	try {
